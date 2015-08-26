25f720cde801657ee52f90d7ef2f46a3e5e2467c
Code Review <admin@git.lc>
2015-08-26 12:07:34 -0400

    Fixing issue(s): 2:3 You must use "/**" style comments for a file comment 2:3 Inline comments must end in full-stops, exclamation marks, or question marks 11:207 Expected 1 newline at end of file; 0 found 11:3 Language constructs must be followed by a single space; expected 1 space but found 2 11:207 Whitespace found at end of line 7:1 Whitespace found at end of line

--- themes/novosibirsk/user-login.tpl.php
+++ themes/novosibirsk/user-login.tpl.php
@@ -1,11 +1,14 @@
 <?php
-  // User login form
+  /**
+   * @file
+   * User login form.
+   */
   $form["links"] = array();
   $form["name"]["#theme"] = "user_login_form_name_field";
   $form["pass"]["#theme"] = "user_login_form_pass_field";
   $form["submit"]["#theme"] = "user_login_form_submit_field";
-  
+
   print drupal_render($form['name']);
   print drupal_render($form['pass']);
   print drupal_render($form);
-  print  "<p><a href=\"" . base_path() . "user/register\"><strong>" . t('Create new account') . "</strong></a><br><br><a href=\"" . base_path() . "user/password\">" . t('Request new password') . "</a></p>";  
+  print "<p><a href=\"" . base_path() . "user/register\"><strong>" . t('Create new account') . "</strong></a><br><br><a href=\"" . base_path() . "user/password\">" . t('Request new password') . "</a></p>";

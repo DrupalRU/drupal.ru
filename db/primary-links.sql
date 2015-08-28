d8f35c8aa8d6db14d4bf41b63be2f49878d4c58a
Gor Martsen <Gor.martsen@gmail.com>
2015-08-27 21:07:42 -0400

    Удаляю префикс с базы.

--- db/primary-links.sql
+++ db/primary-links.sql
@@ -16,24 +16,24 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `dru_menu_links`
+-- Dumping data for table `menu_links`
 --
 -- WHERE:  menu_name='primary-links'
 
-LOCK TABLES `dru_menu_links` WRITE;
-/*!40000 ALTER TABLE `dru_menu_links` DISABLE KEYS */;
-INSERT INTO `dru_menu_links` VALUES ('primary-links',70,0,'tracker','tracker','Новое на сайте','a:0:{}','system',0,0,0,0,-50,1,1,70,0,0,0,0,0,0,0,0,0);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',113,0,'forum','forum','Форум','a:0:{}','menu',0,0,0,0,-49,1,1,113,0,0,0,0,0,0,0,0,0);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',114,0,'blog','blog','Блоги','a:1:{s:10:\"attributes\";a:1:{s:5:\"title\";s:0:\"\";}}','menu',0,0,0,0,-46,1,1,114,0,0,0,0,0,0,0,0,0);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',115,0,'weblink','','ссылки','a:0:{}','menu',1,0,0,0,0,1,1,115,0,0,0,0,0,0,0,0,1);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',116,0,'http://api.drupal.ru','','API','a:0:{}','menu',0,1,0,0,-47,1,1,116,0,0,0,0,0,0,0,0,1);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',117,0,'sitemap','','карта сайта','a:0:{}','menu',1,0,0,0,10,1,1,117,0,0,0,0,0,0,0,0,1);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',118,0,'node/5022','node/%','Документация','a:0:{}','menu',1,0,0,0,-45,1,1,118,0,0,0,0,0,0,0,0,1);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',227,0,'node/7741','node/%','Скачать','a:0:{}','menu',1,0,0,0,-45,1,1,227,0,0,0,0,0,0,0,0,1);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',2259,0,'http://groups.drupal.ru','','Группы','a:1:{s:10:\"attributes\";a:1:{s:5:\"title\";s:39:\"Дискуссионные группы\";}}','menu',1,1,0,0,-48,1,1,2259,0,0,0,0,0,0,0,0,0);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',2439,0,'aggregator/categories/1','aggregator/categories/%','Планета','a:1:{s:10:\"attributes\";a:1:{s:5:\"title\";s:0:\"\";}}','menu',0,0,0,0,30,1,1,2439,0,0,0,0,0,0,0,0,0);
-INSERT INTO `dru_menu_links` VALUES ('primary-links',2548,0,'http://podcasts.drupal.ru','','Подкасты','a:1:{s:10:\"attributes\";a:1:{s:5:\"title\";s:0:\"\";}}','menu',0,1,0,0,-5,1,1,2548,0,0,0,0,0,0,0,0,0);
-/*!40000 ALTER TABLE `dru_menu_links` ENABLE KEYS */;
+LOCK TABLES `menu_links` WRITE;
+/*!40000 ALTER TABLE `menu_links` DISABLE KEYS */;
+INSERT INTO `menu_links` VALUES ('primary-links',70,0,'tracker','tracker','Новое на сайте','a:0:{}','system',0,0,0,0,-50,1,1,70,0,0,0,0,0,0,0,0,0);
+INSERT INTO `menu_links` VALUES ('primary-links',113,0,'forum','forum','Форум','a:0:{}','menu',0,0,0,0,-49,1,1,113,0,0,0,0,0,0,0,0,0);
+INSERT INTO `menu_links` VALUES ('primary-links',114,0,'blog','blog','Блоги','a:1:{s:10:\"attributes\";a:1:{s:5:\"title\";s:0:\"\";}}','menu',0,0,0,0,-46,1,1,114,0,0,0,0,0,0,0,0,0);
+INSERT INTO `menu_links` VALUES ('primary-links',115,0,'weblink','','ссылки','a:0:{}','menu',1,0,0,0,0,1,1,115,0,0,0,0,0,0,0,0,1);
+INSERT INTO `menu_links` VALUES ('primary-links',116,0,'http://api.drupal.ru','','API','a:0:{}','menu',0,1,0,0,-47,1,1,116,0,0,0,0,0,0,0,0,1);
+INSERT INTO `menu_links` VALUES ('primary-links',117,0,'sitemap','','карта сайта','a:0:{}','menu',1,0,0,0,10,1,1,117,0,0,0,0,0,0,0,0,1);
+INSERT INTO `menu_links` VALUES ('primary-links',118,0,'node/5022','node/%','Документация','a:0:{}','menu',1,0,0,0,-45,1,1,118,0,0,0,0,0,0,0,0,1);
+INSERT INTO `menu_links` VALUES ('primary-links',227,0,'node/7741','node/%','Скачать','a:0:{}','menu',1,0,0,0,-45,1,1,227,0,0,0,0,0,0,0,0,1);
+INSERT INTO `menu_links` VALUES ('primary-links',2259,0,'http://groups.drupal.ru','','Группы','a:1:{s:10:\"attributes\";a:1:{s:5:\"title\";s:39:\"Дискуссионные группы\";}}','menu',1,1,0,0,-48,1,1,2259,0,0,0,0,0,0,0,0,0);
+INSERT INTO `menu_links` VALUES ('primary-links',2439,0,'aggregator/categories/1','aggregator/categories/%','Планета','a:1:{s:10:\"attributes\";a:1:{s:5:\"title\";s:0:\"\";}}','menu',0,0,0,0,30,1,1,2439,0,0,0,0,0,0,0,0,0);
+INSERT INTO `menu_links` VALUES ('primary-links',2548,0,'http://podcasts.drupal.ru','','Подкасты','a:1:{s:10:\"attributes\";a:1:{s:5:\"title\";s:0:\"\";}}','menu',0,1,0,0,-5,1,1,2548,0,0,0,0,0,0,0,0,0);
+/*!40000 ALTER TABLE `menu_links` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 

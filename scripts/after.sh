2937268d82fbdc947c4a968955a84fc9dfd980e9
Gor Martsen <Gor.martsen@gmail.com>
2015-08-28 23:47:29 -0400

    Fix deploy scripts. Replace roles

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-SITEPATH="$GITLC_DOCROOT"
+SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
+cd SITEPATH
 
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
-
-echo "Migrating Menu structure"
-mysql -u$SETTINGS_DATABASE_USER -p$SETTINGS_DATABASE_PASS $SETTINGS_DATABASE_NAME < $GITLC_DEPLOY_DIR/db/primary-links.sql
-
-mysql -u$SETTINGS_DATABASE_USER -p$SETTINGS_DATABASE_PASS $SETTINGS_DATABASE_NAME < $GITLC_DEPLOY_DIR/db/secondary-links.sql
-
 
 echo "Clean cache"
 drush cc all
---
+++ scripts/before.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
+cd SITEPATH
+drush cache-clear drush

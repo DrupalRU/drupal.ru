dba9807986f196eee60606664b98852486f707c3
Gor Martsen <Gor.martsen@gmail.com>
2015-10-16 13:31:48 -0400

    Prepare deployment

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "INIT DEVEL Drupal.ru Version"
 
-CORE='drupal-6'
+CORE='drupal-7'
 SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 CONTRIB="acl bbcode bueditor cacherouter captcha  comment_notify comment_upload diff fasttoggle flag flag_abuse geshifilter google_plusone gravatar imageapi imagecache imagecache_profiles live_translation noindex_external_links pathauto pearwiki_filter privatemsg quote simplenews smtp spambot tagadelic taxonomy_manager token transliteration  views xmlsitemap "
 
@@ -37,68 +37,12 @@ drush -y en imageapi_imagemagick flag_actions geshinode imagecache_ui pm_block_u
 echo "Install drupal.ru modules"
 mkdir -p $SITEPATH/sites/all/modules/local
 
-ln -s $GITLC_DEPLOY_DIR/modules/* $SITEPATH/sites/all/modules/local/
+#ln -s $GITLC_DEPLOY_DIR/modules/* $SITEPATH/sites/all/modules/local/
 
 echo "Install drupal.ru themes"
 mkdir -p $SITEPATH/sites/all/themes/local
 
-ln -s $GITLC_DEPLOY_DIR/themes/* $SITEPATH/sites/all/themes/local/
-
-
-# Find all info files
-modules_enable="";
-for file in $(find $GITLC_DEPLOY_DIR -name \*.info -print)
-do
-  filename=$(basename "$file")
-  modules_enable+="${filename%.*},"
-done
-
-echo "Enable modules and themes: $modules_enable"
-drush -y en $modules_enable
-
-echo "Set novosibirsk theme as default"
-drush vset theme_default novosibirsk
-echo "Set garland theme as admin theme"
-drush vset admin_theme garland
-
-if [ -n "SETTINGS_DEVEL" ]; then
-  drush dl devel
-  drush en devel -y
-  drush en devel_generate -y
-  drush generate-content 200
-  drush generate-users 100
-fi  
-
-echo "Migrating Menu structure"
-
-mkdir -p $SITEPATH/sites/all/modules/github
-cd  $SITEPATH/sites/all/modules/github
-
-git clone https://github.com/itpatrol/drupal_deploy.git
-cd drupal_deploy
-git checkout 6.x
-
-cd  $SITEPATH
-drush -y en drupal_deploy
-
-echo "Import roles"
-drush ddi roles --file=$GITLC_DEPLOY_DIR/data/roles.export
-
-echo "Import filters"
-drush ddi filters --file=$GITLC_DEPLOY_DIR/data/filters.export
-
-echo "Import menu structure"
-drush ddi menu --file=$GITLC_DEPLOY_DIR/data/menu_links.export
-
-echo "Import blocks"
-drush ddi blocks --file=$GITLC_DEPLOY_DIR/data/novosibirsk.blocks.export
-
-echo "Import nodetypes"
-drush ddi node_types --file=$GITLC_DEPLOY_DIR/data/node_types.export
-
-echo "Import taxonomy"
-drush ddi taxonomy --file=$GITLC_DEPLOY_DIR/data/taxonomy.export
-
+#ln -s $GITLC_DEPLOY_DIR/themes/* $SITEPATH/sites/all/themes/local/
 
 
 echo "Please check http://$SETTINGS_DOMAIN"

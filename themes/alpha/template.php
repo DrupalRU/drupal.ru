49d584e5307ebe795728f395658cfc4298e51a0f
Gor Martsen <Gor.martsen@gmail.com>
2015-10-17 22:10:25 -0400

    #23 debug code

--- themes/alpha/template.php
+++ themes/alpha/template.php
@@ -4,3 +4,16 @@
  * @file
  * template.php
  */
+
+/**
+ * Process variables for user-picture.tpl.php.
+ *
+ * The $variables array contains the following arguments:
+ * - $account: A user, node or comment object with 'name', 'uid' and 'picture'
+ *   fields.
+ *
+ * @see user-picture.tpl.php
+ */
+function alpha_preprocess_user_picture(&$variables) {
+  print_r($variables);
+}

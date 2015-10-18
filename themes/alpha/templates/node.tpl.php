36ccccfb9f2ced298cb226650a19ce79108f4295
Gor Martsen <Gor.martsen@gmail.com>
2015-10-18 11:51:22 -0400

    #23 pull right node info

--- themes/alpha/templates/node.tpl.php
+++ themes/alpha/templates/node.tpl.php
@@ -82,7 +82,7 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="row">
-    <div  class="col-xs-12 col-sm-3" >
+    <div  class="col-xs-12 col-sm-3 pull-right" >
       <?php if ($display_submitted): ?>
         <div class="submitted">
           <?php print $submitted; ?>

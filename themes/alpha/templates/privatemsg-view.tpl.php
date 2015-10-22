d0955704be93591b478c762955abbe0b91314c9a
Gor Martsen <Gor.martsen@gmail.com>
2015-10-22 18:47:37 -0400

    #23 Style private messages

---
+++ themes/alpha/templates/privatemsg-view.tpl.php
@@ -0,0 +1,22 @@
+<article class="media <?php if ($new) print "bg-info" ?> <?php if ( !empty($message_classes)) { echo implode(' ', $message_classes);} ?> clearfix"  id="privatemsg-mid-<?php print $mid; ?>">
+  <?php if($author_picture): ?>
+  <div class="pull-left">
+    <?php print $author_picture; ?>
+  </div>
+  <?php endif; ?>  
+  <div class="media-body">
+    <span class="text-muted pull-right">
+        <small class="text-muted"><?php print $message_timestamp; ?></small>
+    </span>
+    <strong class="text-success"><?php print $author_name_link; ?></strong>
+    <?php print $anchors; ?>
+    <div class="content">
+      <?php print $message_body; ?>
+    </div>
+    <?php if (isset($message_actions)): ?>
+      <div class="pull-right">
+      <?php print $message_actions ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</article>

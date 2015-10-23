f82385ddaf4522c5c6efaa51abc4f323c563d2df
Gor Martsen <Gor.martsen@gmail.com>
2015-10-23 19:15:28 -0400

    #23 Add  Drupal logo and website name

--- themes/alpha/templates/page--blog--user.tpl.php
+++ themes/alpha/templates/page--blog--user.tpl.php
@@ -76,6 +76,16 @@
 
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="navbar-header">
+    <?php if ($logo): ?>
+    <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+      <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+    </a>
+    <?php endif; ?>
+
+    <?php if (!empty($site_name)): ?>
+    <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+    <?php endif; ?>
+
     <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
       <span class="sr-only">Toggle navigation</span>

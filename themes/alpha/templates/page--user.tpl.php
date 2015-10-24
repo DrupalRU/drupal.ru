c9074d540154b8649a6d2e7d26b14c089f560bd8
Gor Martsen <Gor.martsen@gmail.com>
2015-10-23 20:46:09 -0400

    #23  УБираем заголовок и крошки

--- themes/alpha/templates/page--user.tpl.php
+++ themes/alpha/templates/page--user.tpl.php
@@ -146,13 +146,7 @@
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>

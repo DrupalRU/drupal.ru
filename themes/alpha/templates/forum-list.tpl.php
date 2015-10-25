b206a04fde8deed2e569f4b32ce99c9a64d30c96
Gor Martsen <Gor.martsen@gmail.com>
2015-10-24 22:13:10 -0400

    #23 заменяем на bootstrap grid

--- themes/alpha/templates/forum-list.tpl.php
+++ themes/alpha/templates/forum-list.tpl.php
@@ -33,19 +33,10 @@
  * @ingroup themeable
  */
 ?>
-<table id="forum-<?php print $forum_id; ?>">
-  <thead>
-    <tr>
-      <th><?php print t('Forum'); ?></th>
-      <th><?php print t('Topics');?></th>
-      <th><?php print t('Posts'); ?></th>
-      <th><?php print t('Last post'); ?></th>
-    </tr>
-  </thead>
-  <tbody>
+<div id="forum-<?php print $forum_id; ?>">
   <?php foreach ($forums as $child_id => $forum): ?>
-    <tr id="forum-list-<?php print $child_id; ?>" class="<?php print $forum->zebra; ?>">
-      <td <?php print $forum->is_container ? 'colspan="4" class="container"' : 'class="forum"'; ?>>
+    <div id="forum-list-<?php print $child_id; ?>" class="row <?php print $forum->zebra; ?>">
+      <div <?php print $forum->is_container ? 'colspan="4" class="container col-xs-12"' : 'class="forum col-xs-12 col-sm-8"'; ?>>
         <?php /* Enclose the contents of this cell with X divs, where X is the
                * depth this forum resides at. This will allow us to use CSS
                * left-margin for indenting.
@@ -64,19 +55,18 @@
             <div class="description"><?php print $forum->description; ?></div>
           <?php endif; ?>
         <?php print str_repeat('</div>', $forum->depth); ?>
-      </td>
+      </div>
       <?php if (!$forum->is_container): ?>
-        <td class="topics">
+        <div class="topics col-xs-3 col-sm-1">
           <?php print $forum->num_topics ?>
           <?php if ($forum->new_topics): ?>
             <br />
             <a href="<?php print $forum->new_url; ?>"><?php print $forum->new_text; ?></a>
           <?php endif; ?>
-        </td>
-        <td class="posts"><?php print $forum->num_posts ?></td>
-        <td class="last-reply"><?php print $forum->last_reply ?></td>
+        </div>
+        <div class="posts  col-xs-3 col-sm-1 "><?php print $forum->num_posts ?></div>
+        <div class="last-reply col-xs-6 col-sm-2"><?php print $forum->last_reply ?></div>
       <?php endif; ?>
     </tr>
   <?php endforeach; ?>
-  </tbody>
-</table>
+</div>

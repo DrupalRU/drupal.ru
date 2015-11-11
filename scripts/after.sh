67a8e7f64dd6faa03920f76b8a35762060b15044
Gor Martsen <Gor.martsen@gmail.com>
2015-11-10 20:52:38 -0500

    Упс. Вот так надо

--- scripts/after.sh
+++ scripts/after.sh
@@ -6,6 +6,8 @@ echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
 #Fix view node revisions - issue #58
+drush dis quote -y
+rm -f $SITEPATH/sites/all/modules/local/quote
  
 drush dl quote-7.x-1.x-dev -y
 drush en quote
--- scripts/before.sh
+++ scripts/before.sh
@@ -3,8 +3,5 @@
 SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 cd $SITEPATH
 
-drush dis quote -y
-
-rm -f $SITEPATH/sites/all/modules/local/quote
 
 drush cache-clear drush

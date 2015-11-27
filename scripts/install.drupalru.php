5f1c03ea8812e408168ee8f904a5b68fb9f2982e
Gor Martsen <Gor.martsen@gmail.com>
2015-11-27 14:46:16 -0500

    #62 основа для инсталл скрипта.

---
+++ scripts/install.drupalru.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+
+$data['github_path'] = get_promt_answer('GITHUB DIR');  
+$data['site_path'] = get_promt_answer('DOCROOT');
+$data['mysql_user'] = get_promt_answer('MySQL User');
+$data['mysql_db'] = get_promt_answer('MySQL DB');
+$data['mysql_pass'] = get_promt_answer('MySQL Password');
+
+print_r($data);
+
+function get_promt_answer($promt){
+  if (PHP_OS == 'WINNT' or !function_exists('readline')) {
+    echo $promt .': ';
+    $line = stream_get_line(STDIN, 1024, PHP_EOL);
+  } else {
+    $line = readline($promt . ': ');
+  }
+  return $line;
+}

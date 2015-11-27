675f7c0f59d5b59b88faf494b2655b22714bc39d
Gor Martsen <Gor.martsen@gmail.com>
2015-11-27 14:51:58 -0500

    #62 Добавляем версию ядра и список контриб модулей

--- scripts/install.drupalru.php
+++ scripts/install.drupalru.php
@@ -7,6 +7,12 @@ $data['mysql_user'] = get_promt_answer('MySQL User');
 $data['mysql_db'] = get_promt_answer('MySQL DB');
 $data['mysql_pass'] = get_promt_answer('MySQL Password');
 
+// Core version.
+$data['core'] = 'drupal-7';
+
+// Contrib modules list.
+$data['contrib'] = 'acl bbcode bueditor captcha  comment_notify diff-7.x-3.x-dev fasttoggle geshifilter google_plusone gravatar imageapi noindex_external_links pathauto privatemsg simplenews smtp spambot tagadelic taxonomy_manager jquery_ui jquery_update token rrssb ajax_comments fontawesome transliteration libraries views xmlsitemap bootstrap_lite xbbcode ban_user';
+
 print_r($data);
 
 function get_promt_answer($promt){

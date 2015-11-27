e2c3423e78f8dc77569c2bf80997e5739c644741
Gor Martsen <Gor.martsen@gmail.com>
2015-11-27 15:17:15 -0500

    #62 Разворачивание сайта через drush

--- scripts/install.drupalru.php
+++ scripts/install.drupalru.php
@@ -8,9 +8,14 @@ $data['site_path'] = get_promt_answer('DOCROOT');
 $data['mysql_user'] = get_promt_answer('MySQL User');
 $data['mysql_db'] = get_promt_answer('MySQL DB');
 $data['mysql_pass'] = get_promt_answer('MySQL Password');
+$data['domain'] = get_promt_answer('Domain');
+$data['account_name'] = get_promt_answer('Drupal User name');
+$data['account_email'] = get_promt_answer('Drupal User email');
+$data['account_pass'] = get_promt_answer('Drupal User Password');
 
 // Core version.
 $data['core'] = 'drupal-7';
+$data['site_name'] = 'Drupal.ru Dev version';
 
 // Contrib modules list.
 $data['contrib'] = 'acl bbcode bueditor captcha  comment_notify diff-7.x-3.x-dev fasttoggle geshifilter google_plusone gravatar imageapi noindex_external_links pathauto privatemsg simplenews smtp spambot tagadelic taxonomy_manager jquery_ui jquery_update token rrssb ajax_comments fontawesome transliteration libraries views xmlsitemap bootstrap_lite xbbcode ban_user';
@@ -30,6 +35,8 @@ exec('rsync -a ' . $data['site_path'] . '/drupal/ ' . $data['site_path']);
 exec('rm -rf ' . $data['site_path'] . '/drupal');
 
 echo "Install DRUPAL\n";
+
+exec('/usr/bin/drush site-install standard -y --root=' . $data['site_path'] . ' --account-name=' . $data['account_name'] . ' --account-mail=' . $data['account_email'] . ' --account-pass=' . $data['account_pass'] . ' --uri=http://' . $data['domain'] . ' --site-name="' . $data['site_name'] . '" --site-mail=' . $data['account_email'] . ' --db-url=mysql://' . $data['mysql_user'] . ':' . $data['mysql_pass'] . '@localhost/' . $data['mysql_db']);
 
 function get_promt_answer($promt){
   if (PHP_OS == 'WINNT' or !function_exists('readline')) {

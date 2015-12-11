1e6c6426847504fb36304775be5e9529031c1be8
Anton <awd.com.ua@gmail.com>
2015-12-11 19:29:01 +0200

    Update after.sh

--- scripts/after.sh
+++ scripts/after.sh
@@ -5,14 +5,6 @@ SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
-#Issue #167 replace favicon
-
-echo "Activate module: dru_comment_quote"
-
-ln -s $GITLC_DEPLOY_DIR/modules/dru_comment_quote $SITEPATH/sites/all/modules/local/
-
-drush  en dru_comment_quote -y
-
 #Issue #188 enable HTML-mail modules
 
 echo "Enable module: smtp"

6db8e50ecfb142382d7890655b87bb72e170dbef
Gor Martsen <Gor.martsen@gmail.com>
2015-12-19 16:48:32 -0500

    Подготовка к обновлению 25 декабря 2015

--- scripts/after.sh
+++ scripts/after.sh
@@ -5,6 +5,11 @@ SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
+#update 25 dec 2015
+
+ln -s $GITLC_DEPLOY_DIR/modules/validate_api $SITEPATH/sites/all/modules/local/
+drush -y en user_filter user_filter_notify validate_api antinoob_validate antiswearing_validate
+
 
 echo "Clean cache"
 drush cc all

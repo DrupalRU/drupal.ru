31ed2e4e966955a9edb0136fa563e5a17cbf82e5
Gor Martsen <Gor.martsen@gmail.com>
2015-12-20 11:47:39 -0500

    #124 Обновление переводов

--- scripts/after.sh
+++ scripts/after.sh
@@ -10,6 +10,9 @@ cd $SITEPATH
 ln -s $GITLC_DEPLOY_DIR/modules/validate_api $SITEPATH/sites/all/modules/local/
 drush -y en user_filter user_filter_notify validate_api antinoob_validate antiswearing_validate
 
+# import translation
+drush -y language-import ru $GITLC_DEPLOY_DIR/modules/user_filter/user_filter_notify/translations/user_filter_notify.ru.po
+
 
 echo "Clean cache"
 drush cc all

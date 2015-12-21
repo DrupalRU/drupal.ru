5b970edd5a9378edb9431e33f4dbb5ae139262a1
Code Review <admin@git.lc>
2015-12-20 21:15:56 -0500

    Fixing issue(s): 3:2 There must be exactly one blank line after the file comment 24:1 Line indented incorrectly; expected 6 spaces, found 0 31:1 Line indented incorrectly; expected 6 spaces, found 0 43:1 Line indented incorrectly; expected 6 spaces, found 0 44:1 Line indented incorrectly; expected 6 spaces, found 0 44:1 Inline comments must end in full-stops, exclamation marks, or question marks 51:1 Line indented incorrectly; expected 6 spaces, found 0 51:1 Whitespace found at end of line 52:1 Line indented incorrectly; expected 7 spaces, found 1 53:1 Line indented incorrectly; expected 7 spaces, found 1 53:1 Whitespace found at end of line 54:1 Line indented incorrectly; expected 7 spaces, found 1 55:1 Line indented incorrectly; expected 7 spaces, found 1 56:1 Line indented incorrectly; expected 7 spaces, found 1 56:1 Whitespace found at end of line 57:1 Line indented incorrectly; expected 7 spaces, found 1 58:1 Line indented incorrectly; expected 7 spaces, found 1 59:1 Line indented incorrectly; expected 7 spaces, found 1 59:1 Whitespace found at end of line 60:1 Line indented incorrectly; expected 7 spaces, found 1 61:1 Line indented incorrectly; expected 7 spaces, found 1 61:1 Whitespace found at end of line 62:1 Line indented incorrectly; expected 7 spaces, found 1 62:1 Whitespace found at end of line 63:1 Line indented incorrectly; expected 7 spaces, found 1 64:1 Line indented incorrectly; expected 7 spaces, found 1 65:1 Line indented incorrectly; expected 7 spaces, found 1 66:1 Line indented incorrectly; expected 7 spaces, found 1 77:1 Line indented incorrectly; expected 8 spaces, found 0 77:1 No space found before comment text; expected "// Выделение В Контейнере" but found "//Выделение В Контейнере" 77:1 Whitespace found at end of line 83:1 Line indented incorrectly; expected 8 spaces, found 0 84:1 Line indented incorrectly; expected 8 spaces, found 0 84:1 There must be no blank line following an inline comment 98:1 Line indented incorrectly; expected 8 spaces, found 0 102:1 Line indented incorrectly; expected 6 spaces, found 0 103:1 Line indented incorrectly; expected 7 spaces, found 1 104:1 Line indented incorrectly; expected 7 spaces, found 1 104:1 Whitespace found at end of line 105:1 Line indented incorrectly; expected 7 spaces, found 1 105:1 Whitespace found at end of line 106:1 Line indented incorrectly; expected 7 spaces, found 1 107:1 Line indented incorrectly; expected 7 spaces, found 1 108:1 Line indented incorrectly; expected 7 spaces, found 1 140:1 Line indented incorrectly; expected 6 spaces, found 0 154:1 Line indented incorrectly; expected 6 spaces, found 0 166:1 Line indented incorrectly; expected 6 spaces, found 0 1:1 Missing short description in doc comment 30:1 Whitespace found at end of line 41:7 Space found before semicolon; expected "};" but found "}       ;" 49:1 Functions must not contain multiple empty lines in a row; found 2 empty lines 70:1 Functions must not contain multiple empty lines in a row; found 2 empty lines 81:1 Functions must not contain multiple empty lines in a row; found 2 empty lines 139:1 Whitespace found at end of line 141:4 Doc comment short description must end with a full stop 142:4 There must be exactly one blank line before the tags in a doc comment 143:4 Separate the @param and @returns sections by a blank line. 152:1 Whitespace found at end of line 152:1 Functions must not contain multiple empty lines in a row; found 2 empty lines 153:1 Whitespace found at end of line 155:4 Doc comment short description must end with a full stop 156:4 There must be exactly one blank line before the tags in a doc comment 157:4 Separate the @param and @returns sections by a blank line. 167:4 Doc comment short description must end with a full stop 168:4 There must be exactly one blank line before the tags in a doc comment 170:4 Separate the @param and @returns sections by a blank line.

--- modules/dru_comment_quote/dru-comment-quote.js
+++ modules/dru_comment_quote/dru-comment-quote.js
@@ -21,14 +21,14 @@
         });
       });
 
-// Обработка клика по ссылке "Цитировать"
+      // Обработка клика по ссылке "Цитировать"
       function ClickEventHandler(link) {
         var quote = getQoute(link) || '';
         insertAtCursor(document.getElementsByName("comment_body[und][0][value]")[0], quote);
         return false;
       }
-      
-// Формирует цитируемый текст для вставки в поле комментария
+
+      // Формирует цитируемый текст для вставки в поле комментария
       function formatQuote(element, user_name) {
         var selected_text = element.innerHTML || element.textContent || '';
         if (selected_text !== '') {
@@ -37,52 +37,47 @@
         selected_text = quoteFilter(selected_text);
         selected_text = quoteReplace(selected_text);
         return "[quote=" + user_name + "]" + selected_text + "[/quote]\n";
-      }
-      ;
+      };
 
-// Определяет человекопонятное значение, возвращаемое методом объекта Range
-// Range.compareBoundaryPoints (параметр = key)
+      // Определяет человекопонятное значение, возвращаемое методом объекта Range
+      // Range.compareBoundaryPoints (параметр = key).
       function PointPosition(key) {
         var positions = ['before', 'equal', 'after'];
         return positions[key + 1];
       }
 
-
-/* 
- * Корректирует выделение текста в поле сущности(node, comment)
- * 
- * Если selection внутри container:
- *  Возвращается selection
- * 
- * Если selection за пределами container:
- *  Возвращается container
- *  
- * Если selection и container пересекаются:
- *  Возвращается их пересечение 
- * 
- * @param {DODOMObject} container - комментируемое поле
- * @param {Rannge} selection - выделеный текст
- * @returns {Range}
- */
+      /*
+       * Корректирует выделение текста в поле сущности(node, comment)
+       *
+       * Если selection внутри container:
+       *  Возвращается selection
+       *
+       * Если selection за пределами container:
+       *  Возвращается container
+       *
+       * Если selection и container пересекаются:
+       *  Возвращается их пересечение
+       *
+       * @param {DODOMObject} container - комментируемое поле
+       * @param {Rannge} selection - выделеный текст
+       * @returns {Range}
+       */
       function checkSelection(container, selection) {
         var states = {AFTER: 'after', BEFORE: 'before'}, status = {}, container_range = document.createRange();
         container_range.selectNode(container);
-
 
         status.start_to_start = PointPosition(selection.compareBoundaryPoints(selection.START_TO_START, container_range));
         status.end_to_end = PointPosition(selection.compareBoundaryPoints(selection.END_TO_END, container_range));
         status.start_to_end = PointPosition(selection.compareBoundaryPoints(selection.START_TO_END, container_range));
         status.end_to_start = PointPosition(selection.compareBoundaryPoints(selection.END_TO_START, container_range));
 
-//Выделение В Контейнере 
+        // Выделение В Контейнере
         if (status.start_to_start === states.AFTER && status.end_to_end === states.BEFORE) {
           return selection;
         }
 
-
-// ЧАСТЬ Выделения НЕ в Контейнере
-// Сдвигаем нужную границу Выделения к границе Контейнера
-
+        // ЧАСТЬ Выделения НЕ в Контейнере
+        // Сдвигаем нужную границу Выделения к границе Контейнера
         if (status.end_to_start === states.BEFORE &&
                 status.end_to_end === states.AFTER) {
           selection.setEndAfter(container);
@@ -95,17 +90,17 @@
           return selection;
         }
 
-// Выделение НЕ в КОНТЕЙНЕРЕ
+        // Выделение НЕ в КОНТЕЙНЕРЕ
         return container_range;
 
       }
-/*
- * Возвращает текст цитату, обернутый в тэг цитаты.
- * 
- * @param {type} link - ссылка - инициатор цитирования (Цитировать) 
- *    соответствующего комментария
- * @returns {undefined|String} Цитату
- */
+      /*
+       * Возвращает текст цитату, обернутый в тэг цитаты.
+       *
+       * @param {type} link - ссылка - инициатор цитирования (Цитировать)
+       *    соответствующего комментария
+       * @returns {undefined|String} Цитату
+       */
       function getQoute(link) {
         var source_container, user_name, selection, content, wrapper;
 
@@ -136,12 +131,14 @@
         return formatQuote(wrapper, user_name);
 
       }
-      
-/**
- * Фильтрует цитируемый текст
- * @param {type} text
- * @returns {unresolved} Отфильтрованный текст
- */
+
+      /**
+       * Фильтрует цитируемый текст.
+       *
+       * @param {type} text
+       *
+       * @returns {unresolved} Отфильтрованный текст
+       */
       function quoteFilter(text) {
         var filter = ["\s\[\?\]"];
         for (i = 0; i < filter.lenght; i++) {
@@ -149,13 +146,14 @@
         }
         return text;
       }
- 
- 
-/**
- * Заменяет текст в цитате
- * @param {type} text
- * @returns {unresolved} Текст цитаты
- */
+
+      /**
+       * Заменяет текст в цитате.
+       *
+       * @param {type} text
+       *
+       * @returns {unresolved} Текст цитаты
+       */
       function quoteReplace(text) {
         var data = [];
         for (var i in data) {
@@ -163,12 +161,14 @@
         }
         return text;
       }
-/**
- * Вставляет цитату в место курсора в текстовое поле вормы комментирования
- * @param {type} textArea
- * @param {type} text
- * @returns {undefined}
- */
+      /**
+       * Вставляет цитату в место курсора в текстовое поле вормы комментирования.
+       *
+       * @param {type} textArea
+       * @param {type} text
+       *
+       * @returns {undefined}
+       */
       function insertAtCursor(textArea, text) {
         if (document.selection) {
           textArea.focus();

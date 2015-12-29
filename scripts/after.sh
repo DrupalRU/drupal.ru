cb7cb85d1ee817e21c46503481f0de0143bca066
Gor Martsen <Gor.martsen@gmail.com>
2015-12-28 22:13:06 -0500

    #102 деплой. включаем новый модуль

--- scripts/after.sh
+++ scripts/after.sh
@@ -5,8 +5,10 @@ SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
-#mini update #266 #267
-drush -y language-import ru $GITLC_DEPLOY_DIR/modules/validate_api/antinoob_validate/translations/antinoob_validate.ru.po
+#mini update #102
+ln -s $GITLC_DEPLOY_DIR/modules/dru_forum $SITEPATH/sites/all/modules/local/
+
+drush en -y dru_forum
 
 echo "Clean cache"
 drush cc all

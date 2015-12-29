b89f1b63f7a31822f470c9f6ff427e05060cc73b
Gor Martsen <Gor.martsen@gmail.com>
2015-12-29 14:00:57 -0500

    #258 правка бага стиля при попадании на even строчку

--- scripts/after.sh
+++ scripts/after.sh
@@ -5,9 +5,5 @@ SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
-#mini update #258
-cd $SITEPATH/sites/all/modules/github/alttracker/
-git pull
-
 echo "Clean cache"
 drush cc all

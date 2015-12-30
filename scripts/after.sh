e6251d305e1ce2ac6d0a68b5c8e7613a139fc0bb
Gor Martsen <Gor.martsen@gmail.com>
2015-12-30 18:33:27 -0500

    включаем xbbcode_dru

--- scripts/after.sh
+++ scripts/after.sh
@@ -5,5 +5,10 @@ SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
+# implement #294 xbbcode
+ln -s $GITLC_DEPLOY_DIR/modules/xbbcode_dru $SITEPATH/sites/all/modules/local/
+
+drush -y en xbbcode_dru
+
 echo "Clean cache"
 drush cc all

8a92fd85569b68e440710b877d8617c218ad6b01
Gor Martsen <Gor.martsen@gmail.com>
2015-12-30 19:27:52 -0500

    #259  обновление кода для тега

--- scripts/after.sh
+++ scripts/after.sh
@@ -3,12 +3,10 @@
 SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 
 echo "Full site path: $SITEPATH"
-cd $SITEPATH
 
-# implement #294 xbbcode
-ln -s $GITLC_DEPLOY_DIR/modules/xbbcode_dru $SITEPATH/sites/all/modules/local/
-
-drush -y en xbbcode_dru
+#mini update #259
+cd $SITEPATH/sites/all/modules/github/alttracker/
+git pull
 
 echo "Clean cache"
 drush cc all

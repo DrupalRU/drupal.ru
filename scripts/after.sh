e0cb14de8c0c077b1d89d93a87b2c0f6797fbf50
Gor Martsen <Gor.martsen@gmail.com>
2016-01-18 11:21:51 -0500

    деплой #319.
     Вкл выкл модуля позволяет включить все новые расширения.

--- scripts/after.sh
+++ scripts/after.sh
@@ -4,9 +4,10 @@ SITEPATH="$HOME/domains/$SETTINGS_DOMAIN"
 
 echo "Full site path: $SITEPATH"
 
-#mini update #259
-cd $SITEPATH/sites/all/modules/github/alttracker/
-git pull
+# update #319. Disable Enable is helping to enable all extra xbbcodes to be enabled. Simpler way.
+cd $SITEPATH
+drush -y dis xbbcode_dru
+drush -y en xbbcode_dru
 
 echo "Clean cache"
 drush cc all

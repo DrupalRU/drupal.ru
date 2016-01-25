1231d03bcbc646ae4b70bbafe2174320f3d0e66a
Gor Martsen <Gor.martsen@gmail.com>
2016-01-25 13:07:04 -0500

    #214 стили для тизера компании

--- themes/alpha/templates/node--organization--teaser.tpl.php
+++ themes/alpha/templates/node--organization--teaser.tpl.php
@@ -82,22 +82,16 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php print $user_picture; ?>
-
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
-
   <div class="content"<?php print $content_attributes; ?>>
     <div class="attributes">
+
+      <div class="name">
+        <?php print render($title_prefix); ?>
+        <?php if (!$page): ?>
+          <h3<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+      </div>
 
       <div class="logo">
       <?php print render($content['logo']); ?>
@@ -120,12 +114,14 @@
       </div>
       
     </div>
+    <div class="details">
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
       print render($content);
     ?>
+    <div class="details">
   </div>
 
   <?php print render($content['links']); ?>

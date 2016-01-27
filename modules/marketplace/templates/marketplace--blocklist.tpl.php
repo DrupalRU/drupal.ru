bb30f1bc0b6b24f31f9cb8f1d56fad9a34a069c9
Gor Martsen <Gor.martsen@gmail.com>
2016-01-27 12:33:31 -0500

    debug code

--- modules/marketplace/templates/marketplace--blocklist.tpl.php
+++ modules/marketplace/templates/marketplace--blocklist.tpl.php
@@ -2,4 +2,10 @@
   <?php render($nodes); ?>
   <div class="clearfix"></div>
   <?php render($links); ?>
+  <pre>
+  <?php print_r($links) ?>
+  </pre>
+  <pre>
+  <?php print_r($content) ?>
+  </pre>
 </div>

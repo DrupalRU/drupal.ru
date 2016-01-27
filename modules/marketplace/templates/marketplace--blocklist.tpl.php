4d35b7f36e6f9f671d7d8d9bbf968458c1d4ee30
Gor Martsen <Gor.martsen@gmail.com>
2016-01-27 12:39:33 -0500

    remove debug code

--- modules/marketplace/templates/marketplace--blocklist.tpl.php
+++ modules/marketplace/templates/marketplace--blocklist.tpl.php
@@ -1,11 +1,5 @@
 <div class="block-organizations clearfix">
-  <?php render($nodes); ?>
+  <?php print render($nodes); ?>
   <div class="clearfix"></div>
-  <?php render($links); ?>
-  <pre>
-  <?php print_r($links) ?>
-  </pre>
-  <pre>
-  <?php print_r($content) ?>
-  </pre>
+  <?php print render($links); ?>
 </div>

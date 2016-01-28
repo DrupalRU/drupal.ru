7daae09acb5f130ed7a158ae35a5bba4787ff1de
Gor Martsen <Gor.martsen@gmail.com>
2016-01-27 22:06:28 -0500

    #214 Deploy script

--- scripts/update/issue-214.sh
+++ scripts/update/issue-214.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Issue 214. Marketplace module.
 
+# Link module
+ln -s $GITLC_DEPLOY_DIR/modules/marketplace $SITEPATH/sites/all/modules/local/
+
 # import vocabulary organizations
 drush ddi taxonomy --file=$GITLC_DEPLOY_DIR/data/organizations.taxonomy.export
 
@@ -12,3 +15,6 @@ drush ddi node_types --file=$GITLC_DEPLOY_DIR/data/organization.node_types.expor
 
 # update block settings
 drush ddi blocks --file=$GITLC_DEPLOY_DIR/data/alpha.blocks.export
+
+#import translation
+drush -y language-import ru sites/all/modules/local/marketplace/translations/marketplace.ru.po

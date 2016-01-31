69015a2fdcbbbbc183fe2557a3d072077e6968be
Gor Martsen <Gor.martsen@gmail.com>
2016-01-31 16:53:17 -0500

    #215 стилизирование ссылок

--- modules/simple_events/templates/upcoming--blocklist.tpl.php
+++ modules/simple_events/templates/upcoming--blocklist.tpl.php
@@ -1,5 +1,7 @@
 <div class="block-simple-events clearfix">
   <?php print render($nodes); ?>
   <div class="clearfix"></div>
-  <?php print render($links); ?>
+  <div class="actions">
+    <?php print render($links); ?>
+  </div>
 </div>

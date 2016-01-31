f7e7c7a04382baa9bf8f5ced6c952f6b4f034b04
Gor Martsen <Gor.martsen@gmail.com>
2016-01-31 16:45:13 -0500

    Перебиваем шаблон

--- themes/alpha/templates/node--simple_event--block.tpl.php
+++ themes/alpha/templates/node--simple_event--block.tpl.php
@@ -2,26 +2,18 @@
   <div class="col-xs-1 image">
     <?php print render($content['event_image']); ?>
   </div>
-  <?php if (isset($content['address'])): ?>
-    <div class="col-xs-3 title">
+  
+    <div class="col-xs-7 title">
       <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
-      <div class="event-type">
-        <?php print render($content['simple_event_type']); ?>
-      </div>
-    </div>
-    <div class="col-xs-4 title">
+      <?php if (isset($content['address'])): ?>
         <div class="address">
           <?php print render($content['address']); ?>
         </div>
-    </div>
-  <?php else: ?>
-    <div class="col-xs-7 title">
-      <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+      <?php else: ?>
       <div class="event-type">
         <?php print render($content['simple_event_type']); ?>
       </div>
     </div>
-  <?php endif; ?>
 
   <div class="col-xs-4 date_time">
     <?php print render($content['datetime']); ?>

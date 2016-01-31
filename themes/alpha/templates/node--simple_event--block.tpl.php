f43324a89791aba33c1d6c8abf1dcdce58b07d34
Gor Martsen <Gor.martsen@gmail.com>
2016-01-31 16:21:29 -0500

    #215 стили событий в блоке

--- themes/alpha/templates/node--simple_event--block.tpl.php
+++ themes/alpha/templates/node--simple_event--block.tpl.php
@@ -1,9 +1,14 @@
 <div id="node-<?php print $node->nid; ?>"  class="row  <?php if(isset($classes)): print $classes; endif;?> <?php print $zebra;?> "<?php if(isset($attributes)): print $attributes; endif;?>>
-  <div class="col-xs-3 image">
+  <div class="col-xs-1 image">
     <?php print render($content['event_image']); ?>
   </div>
-  <div class="col-xs-6 title">
+  <div class="col-xs-8 title">
     <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+    <?php if (isset($content['address'])): ?>
+      <div class="address">
+        <?php print render($content['address']); ?>
+      </div>
+    <?php endif; ?>
     <div class="event-type">
       <?php print render($content['simple_event_type']); ?>
     </div>

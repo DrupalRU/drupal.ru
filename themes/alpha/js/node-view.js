e679d6cb9ba5a1b883aaa82c49a0b658e1d49d2c
Frantsuzzz <frantsuzzz_2008@mail.ru>
2016-01-31 04:38:43 +0300

    issue 342

--- themes/alpha/js/node-view.js
+++ themes/alpha/js/node-view.js
@@ -12,16 +12,21 @@
   Drupal.behaviors.AlphaNodeAffix = {
     attach: function (context) {
 
-      $("#node-details").width($("#node-details").parent().width());
+    $("#node-details").width($("#node-details").parent().width());
 
-      $("#node-details").affix({
-        offset: {
-          top: 250,
-          bottom: function () {
-            return (this.bottom = $('footer').outerHeight(true) + 20);
-          }
+    $("#node-details").affix({
+        offset: {top: function () {
+            // возвращает координату Y родительского .node
+            return (this.top = $("#node-details").closest('.node').offset().top)
+            },
+            bottom: function () {
+                // возвращает расстояние от нижней точки родительского .node до низа окна
+                var node_parent = $("#node-details").closest('.node');
+                var all_height = $(document).height();
+                return (this.bottom = all_height - node_parent.offset().top - node_parent.height())
+            }
         }
-      });
+    });
 
       $(window).on('resize', function(){
         $("#node-details").width($("#node-details").parent().width());

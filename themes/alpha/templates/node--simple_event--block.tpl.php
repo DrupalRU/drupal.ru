369170d4cb1d4ae89a60114b95cc681836fa6dee
Gor Martsen <Gor.martsen@gmail.com>
2016-01-31 16:38:38 -0500

    #215 стили списка ближайших событий

--- themes/alpha/templates/node--simple_event--block.tpl.php
+++ themes/alpha/templates/node--simple_event--block.tpl.php
@@ -3,19 +3,19 @@
     <?php print render($content['event_image']); ?>
   </div>
   <?php if (isset($content['address'])): ?>
-    <div class="col-xs-5 title">
+    <div class="col-xs-3 title">
       <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
       <div class="event-type">
         <?php print render($content['simple_event_type']); ?>
       </div>
     </div>
-    <div class="col-xs-3 title">
+    <div class="col-xs-4 title">
         <div class="address">
           <?php print render($content['address']); ?>
         </div>
     </div>
   <?php else: ?>
-    <div class="col-xs-8 title">
+    <div class="col-xs-7 title">
       <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
       <div class="event-type">
         <?php print render($content['simple_event_type']); ?>
@@ -23,7 +23,7 @@
     </div>
   <?php endif; ?>
 
-  <div class="col-xs-3 date_time">
+  <div class="col-xs-4 date_time">
     <?php print render($content['datetime']); ?>
   </div>
 </div>

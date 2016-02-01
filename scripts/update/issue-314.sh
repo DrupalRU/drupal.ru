6e748312380b0fe847a24a29d2ad7a7dcb64a972
Gor Martsen <Gor.martsen@gmail.com>
2016-01-31 20:22:38 -0500

    #314 папка не соответствует названию модуля
    ru_ticket -> dru_tikets

--- scripts/update/issue-314.sh
+++ scripts/update/issue-314.sh
@@ -2,7 +2,7 @@
 
 echo "Activate module: dru_ticket & dru_claim"
 
-ln -s $GITLC_DEPLOY_DIR/modules/dru_ticket $SITEPATH/sites/all/modules/local/
+ln -s $GITLC_DEPLOY_DIR/modules/dru_tickets $SITEPATH/sites/all/modules/local/
 
-drush  en dru_ticket -y
+drush  en dru_tickets -y
 drush  en dru_claim -y

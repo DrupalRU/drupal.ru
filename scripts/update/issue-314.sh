6a3a1f089b5c22693d2fa09e0919a580d1c60b97
Gor Martsen <Gor.martsen@gmail.com>
2016-01-31 20:34:44 -0500

    #314 импорт перевода

--- scripts/update/issue-314.sh
+++ scripts/update/issue-314.sh
@@ -6,3 +6,7 @@ ln -s $GITLC_DEPLOY_DIR/modules/dru_tickets $SITEPATH/sites/all/modules/local/
 
 drush  en dru_tickets -y
 drush  en dru_claim -y
+
+#import translation
+drush -y language-import ru sites/all/modules/local/dru_tickets/translations/dru_tickets.ru.po
+drush -y language-import ru sites/all/modules/local/dru_tickets/dru_claim/translations/dru_claim.ru.po

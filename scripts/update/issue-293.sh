81224ac5899d5dd0e82d887d239f221325cfee86
awd-studio <awd.com.ua@gmail.com>
2016-02-01 18:19:34 +0200

    Со стейджа

--- scripts/update/issue-293.sh
+++ scripts/update/issue-293.sh
@@ -4,4 +4,8 @@ echo "Activate module: dru_tnx"
 
 ln -s $GITLC_DEPLOY_DIR/modules/dru_tnx $SITEPATH/sites/all/modules/local/
 
+#import translation
+
 drush  en dru_tnx -y
+
+drush -y language-import ru sites/all/modules/local/dru_tnx/translations/dru_tnx.ru.po

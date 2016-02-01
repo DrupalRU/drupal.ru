3c74c84a1a098632d85b4c018fe8fe2d20731b94
Gor Martsen <Gor.martsen@gmail.com>
2016-01-31 23:51:33 -0500

    #314 переносим скрипт

--- modules/dru_tickets/dru_tickets.js
+++ modules/dru_tickets/dru_tickets.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.druClaim = {
     attach: function (context, settings) {
       $(document).click(function(e) {
-        if ($(e.target).closest('#claim-add #claim-add-form').length) return;
+        if ($(e.target).closest('#verdict-add #verdict-add-form').length) return;
           $('#claim-add').remove();
           e.stopPropagation();
       });

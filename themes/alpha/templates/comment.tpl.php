f02516ed183786462ddbaaef73635f53ed4f561a
Gor Martsen <Gor.martsen@gmail.com>
2016-02-02 21:31:43 -0500

    очередная попытка выстроенная элементов

--- themes/alpha/templates/comment.tpl.php
+++ themes/alpha/templates/comment.tpl.php
@@ -66,7 +66,9 @@
   </div>
   <?php endif; ?>  
   <div class="media-body">
-    <div style="display: inline-block">
+    <div class="pull-right">
+      <?php print render($content['links']) ?>
+    </div>
     <span class="text-muted pull-right">
         <small class="text-muted"><?php isset($timeago) ? print $timeago : print $changed; ?></small>
         <div href="#" class="actions" data-source="<?php print $comment->cid ?>"><i class="fa fa-ellipsis-h"> </i></div>
@@ -83,10 +85,6 @@
         <?php print $signature; ?>
       </div>
       <?php endif; ?>
-    </div>
-    </div>
-    <div style="display: inline-block">
-    <?php print render($content['links']) ?>
     </div> 
   </div>
 </article>

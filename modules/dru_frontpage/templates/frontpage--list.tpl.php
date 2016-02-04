6e2a67db2185ade57d7d7edeb490689349efc54f
Gor Martsen <Gor.martsen@gmail.com>
2016-02-03 22:55:07 -0500

    #217 Удаляем зависимость от altpager

--- modules/dru_frontpage/templates/frontpage--list.tpl.php
+++ modules/dru_frontpage/templates/frontpage--list.tpl.php
@@ -1,6 +1,4 @@
 <div class="frontpage-list clearfix">
-  <?php print render($content['first-pager']); ?>
   <?php print render($content['nodes']); ?>
-  <?php print render($content['last-pager']); ?>
   <div class="clearfix"></div>
 </div>

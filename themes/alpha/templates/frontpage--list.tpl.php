7384b3f7bbbe21a03a8a0501c128f98ba8db45b7
Gor Martsen <Gor.martsen@gmail.com>
2016-02-04 21:26:20 -0500

    debug code

--- themes/alpha/templates/frontpage--list.tpl.php
+++ themes/alpha/templates/frontpage--list.tpl.php
@@ -1,6 +1,9 @@
 <div class="container">
   <div class="col-xs-12 col-md-4">
-    <h2>SOMETEXT</h2>
+    <h2>Избранное, Новости, Важное</h2>
+    <center>
+      <img src="sites/all/themes/local/alpha/images/blocks/news.png">
+    </center>
   </div>
   <div class="col-xs-12 col-md-8">
     <div class="frontpage-list clearfix">

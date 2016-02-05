93a46ec292cda50edd723dc57424f21f5e57fc3e
Gor Martsen <Gor.martsen@gmail.com>
2016-02-04 21:51:33 -0500

    #217 Deploy ready

--- scripts/update/issue-217.sh
+++ scripts/update/issue-217.sh
@@ -11,5 +11,16 @@ drush vset site_frontpage 'frontpage'
 echo "Activate module: dru_frontpage"
 
 ln -s $GITLC_DEPLOY_DIR/modules/dru_frontpage $SITEPATH/sites/all/modules/local/
-
 drush -y en dru_frontpage
+
+echo "Import settings"
+drush ddi variables --file=$GITLC_DEPLOY_DIR/data/dru_frontpage.variables.export
+
+echo "Set organisations limits"
+drush vset organizations_block_count '4'
+
+echo "Set event limits"
+drush vset upcoming_events_block_count '5'
+
+# update block settings
+drush ddi blocks --file=$GITLC_DEPLOY_DIR/data/alpha.blocks.export

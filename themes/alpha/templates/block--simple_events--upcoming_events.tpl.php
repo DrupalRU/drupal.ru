0b202e67f78042a14c40835b9a7a821b21a36322
Gor Martsen <Gor.martsen@gmail.com>
2016-02-04 20:55:03 -0500

    #217 разбивка под иконки

--- themes/alpha/templates/block--simple_events--upcoming_events.tpl.php
+++ themes/alpha/templates/block--simple_events--upcoming_events.tpl.php
@@ -1,13 +1,16 @@
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="container">
-    <?php print render($title_prefix); ?>
-  <?php if ($block->subject): ?>
-    <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
-  <?php endif;?>
-    <?php print render($title_suffix); ?>
-  
-    <div class="content"<?php print $content_attributes; ?>>
-      <?php print $content ?>
+    <div class="col-xs-12 col-md-4">
+      <?php print render($title_prefix); ?>
+      <?php if ($block->subject): ?>
+        <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+      <?php endif;?>
+      <?php print render($title_suffix); ?>
+    </div>
+    <div class="col-xs-12 col-md-8">
+      <div class="content"<?php print $content_attributes; ?>>
+        <?php print $content ?>
+      </div>
     </div>
   </div>
 </div>
--- themes/alpha/templates/frontpage--list.tpl.php
+++ themes/alpha/templates/frontpage--list.tpl.php
@@ -1,9 +1,14 @@
 <div class="container">
-  <div class="frontpage-list clearfix">
-    <?php print render($content['nodes']); ?>
-    <div class="clearfix"></div>
-    <div class="actions">
-      <?php print render($content['links']); ?>
+  <div class="col-xs-12 col-md-4">
+    <h2>SOMETEXT</h2>
+  </div>
+  <div class="col-xs-12 col-md-8">
+    <div class="frontpage-list clearfix">
+      <?php print render($content['nodes']); ?>
+      <div class="clearfix"></div>
+      <div class="actions">
+        <?php print render($content['links']); ?>
+      </div>
     </div>
   </div>
 </div>

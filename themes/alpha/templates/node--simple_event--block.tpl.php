13886499863dd43c253403b6a30ea8e501bac64a
Gor Martsen <Gor.martsen@gmail.com>
2016-02-05 13:55:51 -0500

    #217 отображение собйтиый в блоках

--- themes/alpha/templates/node--simple_event--block.tpl.php
+++ themes/alpha/templates/node--simple_event--block.tpl.php
@@ -1,9 +1,9 @@
 <div id="node-<?php print $node->nid; ?>"  class="row  <?php if(isset($classes)): print $classes; endif;?> <?php print $zebra;?> "<?php if(isset($attributes)): print $attributes; endif;?>>
-  <div class="col-xs-1 image">
+  <div class="col-xs-2 col-sm-1 image">
     <?php print render($content['event_image']); ?>
   </div>
   
-    <div class="col-xs-7 title">
+    <div class="col-xs-10 col-sm-7 title">
       <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
       <?php if (isset($content['address'])): ?>
         <div class="address">
@@ -15,7 +15,7 @@
       </div>
     </div>
 
-  <div class="col-xs-4 date_time">
+  <div class="col-xs-10 col-xs-offset-2 col-sm-4 date_time">
     <?php print render($content['datetime']); ?>
   </div>
 </div>

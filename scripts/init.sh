203eddc7b0f006f94ffe930b8c17d7a1b5cb7e0b
Gor Martsen <Gor.martsen@gmail.com>
2016-08-27 15:35:22 -0400

    Fix zenci integration

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,36 +2,35 @@
 echo "INIT Drupal.ru"
 
 CORE='drupal-7'
-SITEPATH="$HOME/domains/$DOMAIN"
 CONTRIB="bbcode bueditor captcha  comment_notify diff-7.x-3.x-dev geshifilter gravatar imageapi noindex_external_links pathauto privatemsg simplenews smtp spambot jquery_update token rrssb  fontawesome transliteration libraries bootstrap_lite xbbcode ban_user quote-7.x-1.x-dev l10n_update"
 
-echo "Full site path: $SITEPATH"
+echo "Full site path: $DOCROOT"
 echo "Site core: $CORE"
 echo "Deploy DIR: $ZENCI_DEPLOY_DIR"
 
-cd $SITEPATH
+cd $DOCROOT
 echo "Process make."
 
 drush dl $CORE --drupal-project-rename="drupal"
 
-rsync -a $SITEPATH/drupal/ $SITEPATH
+rsync -a $DOCROOT/drupal/ $DOCROOT
 rm -rf drupal
 
 echo "Install DRUPAL"
 
 if [ "$DATABASE_NAME" == "" ];then
-  /usr/bin/drush site-install standard -y --root=$SITEPATH --account-name=$ACCOUNT_NAME --account-mail=$ACCOUNT_MAIL --account-pass=$ACCOUNT_PASS --uri=http://$DOMAIN --site-name="$SITE_NAME" --site-mail=$SITE_MAIL --db-url=mysql://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME
+  /usr/bin/drush site-install standard -y --root=$DOCROOT --account-name=$ACCOUNT_NAME --account-mail=$ACCOUNT_MAIL --account-pass=$ACCOUNT_PASS --uri=http://$DOMAIN --site-name="$SITE_NAME" --site-mail=$SITE_MAIL --db-url=mysql://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME
 else
 
-  /usr/bin/drush site-install standard -y --root=$SITEPATH --account-name=$ACCOUNT_NAME --account-mail=$ACCOUNT_MAIL --account-pass=$ACCOUNT_PASS --uri=http://$DOMAIN --site-name="$SITE_NAME" --site-mail=$SITE_MAIL --db-url=mysql://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME
+  /usr/bin/drush site-install standard -y --root=$DOCROOT --account-name=$ACCOUNT_NAME --account-mail=$ACCOUNT_MAIL --account-pass=$ACCOUNT_PASS --uri=http://$DOMAIN --site-name="$SITE_NAME" --site-mail=$SITE_MAIL --db-url=mysql://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME
 fi
 
 echo "make libraries dir"
-mkdir $SITEPATH/sites/all/libraries
+mkdir $DOCROOT/sites/all/libraries
 
 echo "Install contrib modules"
 
-mkdir -p $SITEPATH/sites/all/modules/contrib
+mkdir -p $DOCROOT/sites/all/modules/contrib
 drush dl $CONTRIB
 drush -y en $CONTRIB
 
@@ -43,37 +42,37 @@ echo "Install other modules"
 drush -y en imageapi_imagemagick pm_block_user pm_email_notify privatemsg_filter  views_ui book forum
 
 echo "Prepare github modules dir"
-mkdir -p $SITEPATH/sites/all/modules/github
+mkdir -p $DOCROOT/sites/all/modules/github
 
 echo "Install inner poll"
 
-cd $SITEPATH/sites/all/modules/github
+cd $DOCROOT/sites/all/modules/github
 git clone --branch master http://git.drupal.org/sandbox/andypost/1413472.git inner_poll
 cd  inner_poll
 git checkout 7.x-1.x
 
 echo "Deploy module"
 
-cd  $SITEPATH/sites/all/modules/github
+cd  $DOCROOT/sites/all/modules/github
 git clone https://github.com/itpatrol/drupal_deploy.git
 cd drupal_deploy
 git checkout 7.x
 
 echo "Altpager"
-cd  $SITEPATH/sites/all/modules/github
+cd  $DOCROOT/sites/all/modules/github
 git clone https://github.com/itpatrol/altpager
 
 echo "Alttracker"
-cd  $SITEPATH/sites/all/modules/github
+cd  $DOCROOT/sites/all/modules/github
 git clone https://github.com/itpatrol/alttracker
 
-cd $SITEPATH
+cd $DOCROOT
 drush -y en inner_poll altpager alttracker drupal_deploy
 
 echo "Install drupal.ru modules"
-mkdir -p $SITEPATH/sites/all/modules/local
+mkdir -p $DOCROOT/sites/all/modules/local
 
-ln -s $ZENCI_DEPLOY_DIR/modules/* $SITEPATH/sites/all/modules/local/
+ln -s $ZENCI_DEPLOY_DIR/modules/* $DOCROOT/sites/all/modules/local/
 
 echo "Enable quote"
 drush en -y quote
@@ -82,17 +81,17 @@ echo "Enable dru_comment_quote"
 drush en -y dru_comment_quote
 
 echo "Install Font awesome"
-cd  $SITEPATH/sites/all/libraries
+cd  $DOCROOT/sites/all/libraries
 git clone https://github.com/FortAwesome/Font-Awesome.git fontawesome
 
 
 echo "Install drupal.ru themes"
-mkdir -p $SITEPATH/sites/all/themes/local
+mkdir -p $DOCROOT/sites/all/themes/local
 
-ln -s $ZENCI_DEPLOY_DIR/themes/* $SITEPATH/sites/all/themes/local/
+ln -s $ZENCI_DEPLOY_DIR/themes/* $DOCROOT/sites/all/themes/local/
 
 echo "Set default theme"
-cd $SITEPATH
+cd $DOCROOT
 
 echo "Set default variables"
 drush vset theme_default alpha
@@ -155,7 +154,7 @@ drush vset preprocess_css 1
 drush vset preprocess_js 1
 
 if [ "$DEVEL" != "" ]; then
-  cd $SITEPATH
+  cd $DOCROOT
   drush dl devel
   drusn -y en devel devel_generate
   drush generate-content 100

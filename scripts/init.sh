f8124f40dd0d3b7c03d0e4f2ada520437a416b95
Gor Martsen <Gor.martsen@gmail.com>
2016-08-27 17:49:54 -0400

    Use drupalru profile to install

--- scripts/init.sh
+++ scripts/init.sh
@@ -16,30 +16,19 @@ drush dl $CORE --drupal-project-rename="drupal"
 rsync -a $DOCROOT/drupal/ $DOCROOT
 rm -rf drupal
 
-echo "Install DRUPAL"
+echo "Link profile"
 
-/usr/bin/drush site-install standard -y --root=$DOCROOT --account-name=$ACCOUNT_NAME --account-mail=$ACCOUNT_MAIL --account-pass=$ACCOUNT_PASS --uri=http://$DOMAIN --site-name="$SITE_NAME" --site-mail=$SITE_MAIL --db-url=mysqli://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME
+ln -s $ZENCI_DEPLOY_DIR $DOCROOT/profiles/drupalru
 
-echo "make libraries dir"
-mkdir $DOCROOT/sites/all/libraries
-
-echo "Install contrib modules"
+echo "Prepare contrib modules"
 
 mkdir -p $DOCROOT/sites/all/modules/contrib
 drush dl $CONTRIB
-drush -y en $CONTRIB
-
-echo "Install captcha_pack"
-drush dl captcha_pack
-drush -y en ascii_art_captcha css_captcha
-
-echo "Install other modules"
-drush -y en imageapi_imagemagick pm_block_user pm_email_notify privatemsg_filter  views_ui book forum
 
 echo "Prepare github modules dir"
 mkdir -p $DOCROOT/sites/all/modules/github
 
-echo "Install inner poll"
+echo "Download inner poll"
 
 cd $DOCROOT/sites/all/modules/github
 git clone --branch master http://git.drupal.org/sandbox/andypost/1413472.git inner_poll
@@ -61,37 +50,19 @@ echo "Alttracker"
 cd  $DOCROOT/sites/all/modules/github
 git clone https://github.com/itpatrol/alttracker
 
-cd $DOCROOT
-drush -y en inner_poll altpager alttracker drupal_deploy
+echo "make libraries dir"
+mkdir $DOCROOT/sites/all/libraries
 
-echo "Install drupal.ru modules"
-mkdir -p $DOCROOT/sites/all/modules/local
+echo "Install DRUPAL"
 
-ln -s $ZENCI_DEPLOY_DIR/modules/* $DOCROOT/sites/all/modules/local/
+/usr/bin/drush site-install drupalru -y --root=$DOCROOT --account-name=$ACCOUNT_NAME --account-mail=$ACCOUNT_MAIL --account-pass=$ACCOUNT_PASS --uri=http://$DOMAIN --site-name="$SITE_NAME" --site-mail=$SITE_MAIL --db-url=mysqli://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME
 
-echo "Enable quote"
-drush en -y quote
-
-echo "Enable dru_comment_quote"
-drush en -y dru_comment_quote
 
 echo "Install Font awesome"
 cd  $DOCROOT/sites/all/libraries
 git clone https://github.com/FortAwesome/Font-Awesome.git fontawesome
 
-
-echo "Install drupal.ru themes"
-mkdir -p $DOCROOT/sites/all/themes/local
-
-ln -s $ZENCI_DEPLOY_DIR/themes/* $DOCROOT/sites/all/themes/local/
-
-echo "Set default theme"
 cd $DOCROOT
-
-echo "Set default variables"
-drush vset theme_default alpha
-drush vset filestore_tmp_dir /tmp
-drush vset admin_theme alpha
 
 echo "Update translation";
 drush -y dl drush_language
@@ -102,7 +73,6 @@ drush -y l10n-update
 
 #set auto update weekly
 drush vset l10n_update_check_frequency 7
-
 
 
 echo "Import META structure via module http://github.com/itpatrol/drupal_deploy."

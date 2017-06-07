05332fb8327d7b2f1953ab0f646401bf6772967d
Maksim Baev <dev@mbaev.com>
2017-06-07 23:52:15 +0400

    issue #552: Фикс модуля Resolve

--- modules/resolve/resolve.js
+++ modules/resolve/resolve.js
@@ -1,26 +1,36 @@
 (function ($) {
   "use strict";
-
-  /**
-   * Command for resolve a node.
-   *
-   * @param ajax
-   * @param response
-   * @param status
-   */
-  Drupal.ajax.prototype.commands.resolve = function (ajax, response, status) {
-    if (status) {
-      var $pageTitle = $('h1.page-header');
-      if ($pageTitle.length > 0) {
-        if ($pageTitle.children().length > 0) {
-          var $children = $pageTitle.children(),
-            $lastTag = $($children[$children.length - 1]);
-          $lastTag.after('[' + Drupal.t('Resolved') + '] ');
-        }
-        else{
-          $pageTitle.prepend('[' + Drupal.t('Resolved') + '] ');
+  
+  // Hide these in a ready to ensure that Drupal.ajax is set up first.
+  $(function () {
+    /**
+     * Command for resolve a node.
+     *
+     * @param ajax
+     * @param response
+     * @param status
+     */
+    Drupal.ajax.prototype.commands.resolve = function (ajax, response, status) {
+      if (status) {
+        var $pageTitle = $('h1.page-header');
+        if ($pageTitle.length > 0) {
+          if ($pageTitle.children().length > 0) {
+            var $children = $pageTitle.children(),
+              $lastTag = $($children[$children.length - 1]);
+            $lastTag.after(Drupal.theme('resolved'));
+          }
+          else {
+            $pageTitle.prepend(Drupal.theme('resolved'));
+          }
         }
       }
+    };
+  });
+  
+  Drupal.theme.prototype.resolved = function (resolved) {
+    if (typeof resolved == 'undefined' || !resolved) {
+      resolved = true;
     }
+    return resolved ? '[' + Drupal.t('Resolved') + ']' : '';
   };
 })(jQuery);

15a25452e2fb25e883a93a9733e7a6d5508e1621
Gor Martsen <Gor.martsen@gmail.com>
2017-06-29 15:52:26 -0400

    Fix pass export

--- scripts/stage_init.sh
+++ scripts/stage_init.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+export DATABASE_PASS=`cat /home/stage/.my.cnf |grep pass|awk -F= '{print$2}'`
+
+
 #install drupal
 sh $ZENCI_DEPLOY_DIR/scripts/init.sh
 
-export DATABASE_PASS=`cat /home/stage/.my.cnf |grep pass|awk -F= '{print$2}'`
 
 # revert database and files
 mysql --user=$DATABASE_USER --password=$DATABASE_PASS $DATABASE_NAME < $HOME/dump/drupal_main.sql

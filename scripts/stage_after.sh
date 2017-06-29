8de71a143e7b30bad34ab9d00fd2832106a7f70d
Gor Martsen <Gor.martsen@gmail.com>
2017-06-29 15:02:10 -0400

    fix stage import

--- scripts/stage_after.sh
+++ scripts/stage_after.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-
-#install drupal
-sh $ZENCI_DEPLOY_DIR/scripts/init.sh
-
 export DATABASE_PASS=`cat /home/stage/.my.cnf |grep pass|awk -F= '{print$2}'`
 
-# revert database and files
+# revert database
 mysql -u $DATABASE_USER $DATABASE_PASS $DATABASE_NAME < $HOME/dump/drupal_main.sql
 mysql -u $DATABASE_USER $DATABASE_PASS $DATABASE_NAME < $HOME/dump/drupal_main.sphinxmain.sql
 
-cd $DOCROOT
-rm -rf files
-ln -s ~/files ./files
+
+echo "Process after.sh"
+sh $ZENCI_DEPLOY_DIR/scripts/after.sh

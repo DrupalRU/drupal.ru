80a2e4008d1a24d321b0f1cecfba5816427ff428
Gor Martsen <Gor.martsen@gmail.com>
2017-07-04 13:16:45 -0400

    disable revert

--- scripts/stage_after.sh
+++ scripts/stage_after.sh
@@ -2,8 +2,8 @@
 export DATABASE_PASS=`cat /home/stage/.my.cnf |grep pass|awk -F= '{print$2}'`
 
 # revert database
-mysql --user=$DATABASE_USER --password=$DATABASE_PASS $DATABASE_NAME < $HOME/dump/drupal_main.sql
-mysql --user=$DATABASE_USER --password=$DATABASE_PASS $DATABASE_NAME < $HOME/dump/drupal_main.sphinxmain.sql
+#mysql --user=$DATABASE_USER --password=$DATABASE_PASS $DATABASE_NAME < $HOME/dump/drupal_main.sql
+#mysql --user=$DATABASE_USER --password=$DATABASE_PASS $DATABASE_NAME < $HOME/dump/drupal_main.sphinxmain.sql
 
 
 echo "Process after.sh"

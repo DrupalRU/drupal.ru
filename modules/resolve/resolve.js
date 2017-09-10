3c55c09f1c394ea0de1a3467d09bd964f51da2f7
Maksim Baev <dev@mbaev.com>
2017-09-11 01:28:10 +0400

    issue #514: Модуль Resolove - подключение JS

--- modules/resolve/resolve.js
+++ modules/resolve/resolve.js
@@ -1,32 +1,20 @@
 (function ($) {
   "use strict";
-  
-  // Hide these in a ready to ensure that Drupal.ajax is set up first.
-  $(function () {
-    /**
-     * Command for resolve a node.
-     *
-     * @param ajax
-     * @param response
-     * @param status
-     */
-    Drupal.ajax.prototype.commands.resolve = function (ajax, response, status) {
-      if (status) {
-        var $pageTitle = $('h1.page-header');
-        if ($pageTitle.length > 0) {
-          if ($pageTitle.children().length > 0) {
-            var $children = $pageTitle.children(),
-              $lastTag = $($children[$children.length - 1]);
-            $lastTag.after(Drupal.theme('resolved'));
-          }
-          else {
-            $pageTitle.prepend(Drupal.theme('resolved'));
-          }
-        }
-      }
-    };
-  });
-  
+
+  /**
+   * Command for resolve a node.
+   *
+   * @param ajax
+   * @param response
+   * @param status
+   */
+  Drupal.ajax.prototype.commands.resolve = function (ajax, response, status) {
+    if (status) {
+      var $pageTitle = $('h1.page-header'), $children = $pageTitle.children();
+      $pageTitle.text('[' + Drupal.t('Resolved') + '] '+ $pageTitle.text()).prepend($children);
+    }
+  };
+
   Drupal.theme.prototype.resolved = function (resolved) {
     if (typeof resolved == 'undefined' || !resolved) {
       resolved = true;

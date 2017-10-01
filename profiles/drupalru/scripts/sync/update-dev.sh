162547e9a6b4bdf9d151d9b4ba01f708d62eaa1d
Maksim Baev <dev@mbaev.com>
2017-10-01 23:51:47 +0400

    issue #601: Добавить возможность простой локальной развёртки

--- profiles/drupalru/scripts/sync/update-dev.sh
+++ profiles/drupalru/scripts/sync/update-dev.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cd "$HOME/"
-wget http://drupal.ru/sites/default/files/drupalru-dump.sql.gz ./
-drush sql-sync default @dru.dev --no-dump --source-dump="$HOME/drupalru-dump.sql.gz" -y
+wget http://drupal.ru/sites/default/files/drupalru-dump.sql.gz
+drush sql-sync @dru.stage @dru.dev --no-dump --source-dump="$HOME/drupalru-dump.sql.gz" -y
 drush @dru.dev en devel_generate -y
 drush @dru.dev genu 10
 drush @dru.dev genc 100 5 --types=blog

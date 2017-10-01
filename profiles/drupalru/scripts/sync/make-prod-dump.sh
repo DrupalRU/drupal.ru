febf9024ce4351a18f1dead5ccef1ea3505d2bd2
Maksim Baev <dev@mbaev.com>
2017-10-01 20:04:34 +0400

    issue #601: Добавить возможность простой локальной развёртки

--- profiles/drupalru/scripts/sync/make-prod-dump.sh
+++ profiles/drupalru/scripts/sync/make-prod-dump.sh
@@ -2,12 +2,7 @@
 
 drush @dru.prod cc all
 drush @dru.prod wd-del all -y
-drush @dru.prod sql-sync default temp --sanitize -y --structure-tables-list=\
-xmlsitemap,node,node_comment_statistics,alttracker_node,forum,pm_index,\
-node_revision,field_revision_body,taxonomy_index,alttracker_user,profile_value,\
-field_data_comment_body,comment,comment_notify,\
-field_data_taxonomy_vocabulary_7,field_data_taxonomy_vocabulary_2,\
-field_revision_taxonomy_vocabulary_7,field_revision_taxonomy_vocabulary_2
+drush sql-sync @dru.prod @dru.temp --sanitize -y
 drush @dru.temp scr "profiles/drupalru/scripts/sync/sanitize.php"
 drush @dru.temp ucrt admin --password=111
 drush @dru.temp sql-dump --result-file="$HOME/domains/drupal.ru/sites/default/files/drupalru-dump.sql" --gzip

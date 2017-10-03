cbe9ab25254765d708b8a12032b6d695ed401e66
Maksim Baev <dev@mbaev.com>
2017-10-03 22:58:57 +0400

    issue #601: Добавить возможность простой локальной развёртки

--- profiles/drupalru/scripts/sync/make-prod-dump.sh
+++ profiles/drupalru/scripts/sync/make-prod-dump.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 sm() {
   echo "\033[0;32m$@\033[0m";
@@ -9,11 +9,11 @@ exe() {
   $@;
 }
 
-DUMP_DIR="$HOME/domains/drupal.ru/sites/default/files/drupalru-dump.sql"
+DUMP_DIR=$HOME/domains/drupal.ru/sites/default/files/drupalru-dump.sql
 
 exe "drush @dru.prod cc all"
 exe "drush @dru.prod wd-del all -y"
 exe "drush sql-sync @dru.prod @dru.temp --sanitize -y"
-exe "drush @dru.temp scr \"profiles/drupalru/scripts/sync/sanitize.php\""
+exe "drush @dru.temp scr profiles/drupalru/scripts/sync/sanitize.php"
 exe "drush @dru.temp ucrt admin --password=111"
-exe "drush @dru.temp sql-dump --result-file=\"$DUMP_DIR\" --gzip"
+exe "drush @dru.temp sql-dump --result-file=$DUMP_DIR --gzip"

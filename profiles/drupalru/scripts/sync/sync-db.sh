97d9240c51c38fa375923c8617a6c0d6ee6c22ab
Anton Karpov <awd.com.ua@gmail.com>
2017-10-30 09:17:33 +0200

    issue #751: Добавлена проверка на существование почты

--- profiles/drupalru/scripts/sync/sync-db.sh
+++ profiles/drupalru/scripts/sync/sync-db.sh
@@ -55,6 +55,10 @@ exe "drush $DRUSH_ALIAS genu 10"
 exe "drush $DRUSH_ALIAS genc 100 5 --types=blog"
 exe "drush $DRUSH_ALIAS dis devel_generate -y"
 exe "drush $DRUSH_ALIAS pm-uninstall devel_generate -y"
-exe "drush $DRUSH_ALIAS vset site_mail $GIT_EMAIL"
+
+if [ -z "$GIT_EMAIL" ]; then
+  exe "drush $DRUSH_ALIAS vset site_mail $GIT_EMAIL"
+fi
+
 exe "drush $DRUSH_ALIAS cc all"
 exe "drush $DRUSH_ALIAS uli"

9c0307b1faaf33241bb850ba81b07556077ec879
Anton Karpov <awd.com.ua@gmail.com>
2017-11-09 22:04:57 +0200

    issue #666: После загрузки файла комментарий не отправляется

--- profiles/drupalru/themes/druru/js/druru.js
+++ profiles/drupalru/themes/druru/js/druru.js
@@ -409,36 +409,6 @@ var Drupal = Drupal || {};
     }
   };
 
-  Drupal.behaviors.druruBueditor = {
-    attach: function (context, settings) {
-      if (typeof BUE !== 'undefined') {
-
-        var grabbedClass = function () {
-          if (!window.attachedGrab) {
-            var $popupHeader = $(window).find('.bue-popup-head');
-            $popupHeader
-              .on('mousedown', function (e) {
-                $popupHeader.addClass('grabbing');
-              })
-              .on('mouseup', function (e) {
-                $popupHeader.removeClass('grabbing');
-              });
-          }
-          window.attachedGrab = true;
-          return window.sourceOnopen();
-        };
-
-        for (var popup in BUE.popups || {}) {
-          if (BUE.popups.hasOwnProperty(popup)) {
-            var window = BUE.popups[popup];
-            window.sourceOnopen = window.onopen;
-            window.onopen = grabbedClass;
-          }
-        }
-      }
-    }
-  };
-
   Drupal.behaviors.druruBlogTeaserViewSwitcher = {
     attach: function (context, settings) {
       $('.view-switcher').on('click', function () {

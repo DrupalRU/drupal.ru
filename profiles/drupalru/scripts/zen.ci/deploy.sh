422cdb7ef17ef80f3f112a5c1ba9b58d4396252e
Maksim Baev <dev@mbaev.com>
2017-12-03 18:20:53 +0400

    issue #829: Исправить удаление устаревших версий при деплойменте

--- profiles/drupalru/scripts/zen.ci/deploy.sh
+++ profiles/drupalru/scripts/zen.ci/deploy.sh
@@ -46,6 +46,7 @@ if [ $(ls -l | grep -c ^d) -gt $STORE_VERSIONS ] ; then
     while [ $(ls -l | grep -c ^d) -gt $STORE_VERSIONS ]
     do
         DEPRECATED_VERSION=$(ls -r | tail -n 1)
+        chmod 755 -R ./$DEPRECATED_VERSION
         rm -rf ./$DEPRECATED_VERSION
         print_status "Версия \"$DEPRECATED_VERSION\" удалена"
     done

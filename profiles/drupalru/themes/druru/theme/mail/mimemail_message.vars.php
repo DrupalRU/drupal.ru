302224b0247b2780be9c16ae54e5bd94390f07d2
Anton Karpov <awd.com.ua@gmail.com>
2017-12-18 19:54:10 +0200

    Issue #188 + #606: Фиксинг шаблона

--- profiles/drupalru/themes/druru/theme/mail/mimemail_message.vars.php
+++ profiles/drupalru/themes/druru/theme/mail/mimemail_message.vars.php
@@ -5,9 +5,15 @@
  *
  * The $variables array initially contains the following arguments:
  * - $recipient: The recipient of the message
- * - $key:  The mailkey associated with the message
  * - $subject: The message subject
- * - $body:  The message body
+ * - $body: The message body
+ * - $css: Internal style sheets
+ * - $module: The sending module
+ * - $key: The message identifier
+ * - $logo: The site logo for mail
+ * - $menu_top: Main menu
+ * - $menu_bottom: Secondary menu
+ * - $base_path: Site base url
  *
  * @see mimemail-message.tpl.php
  */
@@ -17,7 +23,26 @@ function druru_preprocess_mimemail_message(&$variables) {
   $variables['logo'] = file_exists($logo) ? file_create_url($logo) : null;
   $variables['base_path'] = base_path();
   $variables['menu_top'] = druru_build_menu_for_mail('main-menu');
-  $variables['menu_bottom'] = druru_build_menu_for_mail('main-menu');
+  // ToDo: Uncomment when secondary menu will exist
+  // $variables['menu_bottom'] = druru_build_menu_for_mail('main-menu');
+  $variables['menu_bottom'] = [
+    [
+      'link'  => url('privacy-policy', ['absolute' => true]),
+      'title' => 'Конфиденциальность',
+    ],
+    [
+      'link'  => url('rules', ['absolute' => true]),
+      'title' => 'Правила',
+    ],
+    [
+      'link'  => url('ru_team', ['absolute' => true]),
+      'title' => 'Команда',
+    ],
+    [
+      'link'  => url('contact', ['absolute' => true]),
+      'title' => 'Обратная связь',
+    ],
+  ];
 }
 
 /**

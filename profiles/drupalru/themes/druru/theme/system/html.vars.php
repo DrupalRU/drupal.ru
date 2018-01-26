8bdfffa0e8250df24420d1cbfb319ac5c2ab5e17
Andrey Ivnitsky <info@itcrowd72.ru>
2018-01-27 01:01:48 +0500

    issue #644: Что это за значок?

--- profiles/drupalru/themes/druru/theme/system/html.vars.php
+++ profiles/drupalru/themes/druru/theme/system/html.vars.php
@@ -27,4 +27,10 @@ function druru_preprocess_html(&$variables) {
   // Stylize 404 and 403 pages.
   // Works only in case if in settings of the site not set 403 and 404 page.
   _druru_error_pages_preprocess('html', $vars);
+
+  // Add class forms-help if user set checkbox and for anonimous users
+  $user = $variables['user'];
+  if (!isset($user->data['forms_help']) || $user->data['forms_help']) {
+    $variables['classes_array'][] = 'forms-help';
+  }
 }

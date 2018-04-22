45896662f5098b366b874ddcdab5bb23b29a355e
Andrey Ivnitsky <info@itcrowd72.ru>
2018-04-22 21:14:52 +0500

    issue #1010: Переместить блок «Лучший ответ» под список ссылок материала (links)

--- profiles/drupalru/themes/druru/theme/system/node.tpl.php
+++ profiles/drupalru/themes/druru/theme/system/node.tpl.php
@@ -127,11 +127,15 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
+      hide($content['best-comment-header']);
+      hide($content['resolved_comment']);
       print drupal_render($content);
     ?>
   </div>
 
   <?php print render($content['links']); ?>
+  <?php print render($content['best-comment-header']); ?>
+  <?php print render($content['resolved_comment']); ?>
   <?php print render($content['comments']); ?>
 
 </div>

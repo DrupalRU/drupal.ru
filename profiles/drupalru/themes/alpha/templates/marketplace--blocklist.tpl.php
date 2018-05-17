6a20608e9762e2de1b608344cf038fec9a442a73
Andrey Ivnitsky <info@itcrowd72.ru>
2018-05-18 00:24:53 +0500

    issue #1083: Удалить неиспользуемую тему оформления Alpha

--- profiles/drupalru/themes/alpha/templates/marketplace--blocklist.tpl.php
+++
@@ -1,7 +0,0 @@
-<div class="block-organizations clearfix">
-  <?php print render($nodes); ?>
-  <div class="clearfix"></div>
-  <div class="actions">
-    <?php print render($links); ?>
-  </div>
-</div>

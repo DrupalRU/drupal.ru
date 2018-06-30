84d80b6fb4cb97d08a7a18fa8a2ef9faebd25854
Maksim Baev <dev@mbaev.com>
2018-06-30 21:39:15 +0400

    Fix of sanitization scripts

--- profiles/drupalru/scripts/sync/build-dumps.sh
+++ profiles/drupalru/scripts/sync/build-dumps.sh
@@ -20,7 +20,7 @@ exe "drush @dru.prod wd-del all -y"
 
 sm "Create dumps"
 drush @dru.prod sql-dump --result-file=$DRUPALRU_DUMP_FILE --skip-tables-list=sphinxmain -y
-drush @dru.tmp sqlc < $DRUPALRU_DUMP_FILE
+drush @dru.temp sqlc < $DRUPALRU_DUMP_FILE
 rm $DRUPALRU_DUMP_FILE
 exe "drush @dru.temp scr profiles/drupalru/scripts/sync/dev/sanitize.php"
 drush @dru.temp sql-dump --result-file=$DRUPALRU_DEV_DUMP --gzip > /dev/null 2>&1

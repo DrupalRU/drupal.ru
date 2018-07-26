aefb85a128815ba69d1ee791a307142fffd6d2a0
Andrey Ivnitsky <info@itcrowd72.ru>
2018-07-26 11:40:52 +0500

    issue #1144: Удалить неиспользуемые темы оформления

--- sites/all/themes/contrib/bootstrap_lite/js/navbar-static-top.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file
- * JS fixed top nav bar .
- */
-(function($, Backdrop, window, document, undefined) {
-  $(document).ready(function() {
-    $("#navbar").detach().prependTo('body');
-  });
-})(jQuery, Backdrop, this, this.document);

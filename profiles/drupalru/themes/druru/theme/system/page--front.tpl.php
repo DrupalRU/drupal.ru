7debb4927625a06c51a78a824255c73215ca264e
Andrey Ivnitsky <info@itcrowd72.ru>
2018-09-28 11:45:47 +0500

    issue #1158: Отвязать главную страницу от модуля dru_frontpage

--- profiles/drupalru/themes/druru/theme/system/page--front.tpl.php
+++ profiles/drupalru/themes/druru/theme/system/page--front.tpl.php
@@ -165,7 +165,7 @@
           <?php if ($action_links): ?>
             <ul class="action-links"><?php print render($action_links); ?></ul>
           <?php endif; ?>
-          <?php print render($page['content']); ?>
+          <?php print render($page['frontpage']); ?>
         </section>
 
         <?php if ($page['sidebar_first']): ?>

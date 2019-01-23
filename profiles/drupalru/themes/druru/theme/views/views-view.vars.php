0efb71a0ef6381b86c89a5ee2e019c2d726cb04f
Andrey Ivnitsky <info@itcrowd72.ru>
2019-01-23 12:44:20 +0500

    issue #878: Список комментариев пользователя

--- profiles/drupalru/themes/druru/theme/views/views-view.vars.php
+++ profiles/drupalru/themes/druru/theme/views/views-view.vars.php
@@ -6,7 +6,7 @@
 function druru_preprocess_views_view(&$vars) {
   $view = &$vars['view'];
 
-  if ($view->name == 'comments_user') {
+  if ($view->name == 'user_comments') {
     drupal_add_js(drupal_get_path('theme', 'druru') . '/js/modules/views/js/user-profile-comments.js');
   }
 }

5fa6c98b5c46787cb387e52dd2e98cc756981efc
avakorin <arthur.vakorin@gmail.com>
2019-02-17 19:47:21 +0300

    issue #1276: Bugfix display of nested comments

--- profiles/drupalru/themes/druru/theme/views/comment--view--user-comments--page.tpl.php
+++ profiles/drupalru/themes/druru/theme/views/comment--view--user-comments--page.tpl.php
@@ -60,7 +60,6 @@
  */
 ?>
 <article data-entity-type="comment" class="<?php print $zebra; ?> <?php if ($new) print "is-new" ?> <?php print $classes; ?>"<?php print $attributes; ?>>
-  <a id="comment-<?php print $comment->cid; ?>"></a>
   <h2 class="comment__title"><?php print $title; ?></h2>
 
   <header class="comment__meta">
